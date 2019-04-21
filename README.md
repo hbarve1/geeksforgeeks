9c1cdf5b2f21ca8f769cb80ba6036f349b970552
Himank Barve <hbarve1592@gmail.com>
2019-04-21 20:47:44 +0530

    flow, eslint with jest are working now

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # geeksforgeeks
 
 This repository contains all the problems from https://www.geeksforgeeks.org solved using javascript.
+
+## Objectives
+
+* To Practice Data Structres and Algorithms (ofcourse)
+* To practice TDD as well
+* Configured CI using gitlab runner
+* Use flow typed to document code as well
+* Eslint and Prettier for maintaining the code quality
--- src/array.js
+++ src/array.js
@@ -1,16 +1,31 @@
 /**
- * @format
+ *
+ * @flow
  */
 
 class MyArray {
+  length: number;
+
+  data: {
+    [x: number]: number
+  };
+
   constructor() {
     this.length = 0;
     this.data = {};
   }
 
-  insert(value: Number) {
+  insert(value: number): { [x: number]: number } {
     this.length += 1;
     this.data[this.length] = value;
+
+    return this.data;
+  }
+
+  update(value: number, index: number): { [x: number]: number } {
+    this.data[index] = value;
+
+    return this.data;
   }
 }
 
